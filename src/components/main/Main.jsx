fd07e471b9dfdf50b7822e1903447adad03c6e16
rajesh <peketirajesh007@gmail.com>
2023-09-10 22:05:27 +0530

    errors cleared

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,7 +6,6 @@ import Category from './categories/Category'
 import Offers from './categories/offers/Offers'
 import MainProducts from './main_products/Main_Products'
 import Footer from '../footer/Footer'
-import Categories from './categories/offers/Categories'
 
 const Main = () => {
 
@@ -73,10 +72,10 @@ const Main = () => {
                     </div>
                 </form>
             </div>
-            <Categories />
-            <Offers />
-            <Category />
 
+
+            <Category />
+            <Offers />
             <MainProducts />
             <Footer />
         </>
