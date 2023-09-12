5b2ae38af396f153c2b063321b5a2b60b27b5cb5
rajesh <peketirajesh007@gmail.com>
2023-09-13 01:18:54 +0530

    responsive done on cart container

--- src/components/cart/CartContainer.jsx
+++ src/components/cart/CartContainer.jsx
@@ -11,7 +11,6 @@ const CartContainer = () => {
     const [pincode, setPincode] = useState("")
     //const [cartData, setCartData] = useState([])
     //const [cartValue, setCartValue] = useState([0])
-
     const changeHandler = (e) => {
         setPincode(e.target.value)
     }
@@ -38,14 +37,14 @@ const CartContainer = () => {
         <>
             <div id='cart' className="cart_container h-screen w-[100%] border left-0 right-0  top-0 bg-slate-100 z-[1000]  pt-5 fixed " >
                 <div className='right-10 max-[826px]:right-5 absolute cursor-pointer'><ImCancelCircle size={25} onClick={() => setCart(false)} /></div>
-                <div className="cart_content bg-white h-[100%] pb-[5rem] max-[682px]:overflow-y-scroll mt-[5rem] rounded-t-[2rem] p-5 ">
+                <div className="cart_content bg-white h-[100%] pb-[5rem]  mt-[5rem] rounded-t-[2rem] p-5 ">
                     <h2 className='text-2xl font-medium pb-2  border-b border-slate-500 mb-2'>Shopping cart</h2>
                     <div className="flex gap-5 h-[100%] max-[682px]:flex-col mt-4">
                         <div className=" basis-2/3 ">
-                            <div className="flex justify-between px-12 max-[826px]:px-0 max-[826px]:ps-0 max-[462px]:flex-col  bg-[#ececec] border">
+                            <div className="flex justify-between px-12  max-[983px]:px-0 max-[826px]:ps-0 max-[462px]:flex-col  bg-[#ececec] border">
                                 <p className='py-1 max-[826px]:text-sm max-[826px]:ps-1'><span className='text-slate-600'>Deliver to: </span>{pincodeData.length >= 1 ? <span className='mx-1'>{pincodeData[0].PostOffice[0].Block}</span> : "Enter pincode -->"}</p>
                                 <form onSubmit={submitHandler} className=' max-[826px]:text-sm'>
-                                    <input type="search" name='pincode' value={pincode} placeholder='Get your area details' className='border-0 align-middle px-1 h-[100%] max-[462px]:py-1 max-[826px]:w-36 max-[462px]:w-[75%]' onChange={changeHandler} />
+                                    <input type="search" name='pincode' value={pincode} placeholder='Get your area details' className='border-0 align-middle px-1 h-[100%] max-[462px]:py-1 max-[826px]:w-36 max-[462px]:w-[75%] ' onChange={changeHandler} />
                                     <input type="submit" value="Get area" className='cursor-pointer border-0 align-middle max-[826px]:px-1 px-2 text-orange-500 bg-white h-[100%] max-[462px]:py-[.1rem] max-[462px]:w-[25%]' />
                                 </form>
                             </div>
@@ -57,15 +56,15 @@ const CartContainer = () => {
                                                 {
                                                     cartData[0].map((i, n) => {
                                                         return <div key={n} className='p-2 flex gap-4  '>
-                                                            <img src={i.image} alt="images" className='w-[10rem]' />
+                                                            <img src={i.image} alt="images" className='w-[10rem] max-[826px]:w-[8rem] max-[826px]: ' />
                                                             <div>
-                                                                <h2 className='text-lg'>{i.name}</h2>
-                                                                <p className='text-slate-600'>{i.description}</p>
-                                                                <p className='block my-1'><span className='text-slate-600'>seller: </span>{i.store}</p>
-                                                                <p className='line-through text-slate-600 text-[.8rem]'>₹{Math.ceil(i.price * 100)}</p>
-                                                                <p className='ms-3 text-xl'>₹{Math.ceil((i.price * 100) * 0.9)}</p>
-                                                                <p className='ms-3 text-sm text-green-600'>10% Offer</p><br />
-                                                                <p className='px-2 text-white rounded-sm mt-2 text-sm cursor-pointer bg-orange-300'>Remove</p>
+                                                                <h2 className='text-lg max-[826px]:text-[1rem]'>{i.name}</h2>
+                                                                <p className='text-slate-600 max-[826px]:text-[.8rem]'>{i.description}</p>
+                                                                <p className='block my-1 max-[826px]:text-[.8rem] '><span className='text-slate-600'>seller: </span>{i.store}</p>
+                                                                <p className='line-through max-[826px]:text-[.7rem] text-slate-600 text-[.8rem]'>₹{Math.ceil(i.price * 100)}</p>
+                                                                <p className='ms-3 text-xl max-[826px]:text-lg font-normal'>₹{Math.ceil((i.price * 100) * 0.9)}</p>
+                                                                <p className='ms-3 text-sm text-green-600 max-[826px]:text-[.7rem]'>10% Offer</p><br className='max-[826px]:hidden' />
+                                                                <p className='px-2 text-white rounded-sm mt-2 text-sm cursor-pointer bg-orange-300 max-[826px]:ms-10 max-[520px]:ms-0'>Remove item</p>
                                                             </div>
                                                         </div>
                                                     })
